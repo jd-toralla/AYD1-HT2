731c58cb5ee866088e1ea94b2c0bf3d86cfe5930
Camran1234 <Camran750@gmail.com>
2023-06-12 23:18:16 -0600

    [ADD] info-integrantes

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
 
+app.get('/info', (req, res) => {
+  res.send(`
+    Jonathan Marcos Valiente González-201931581 \n
+    Denilson Florentín de León Aguilar-201830313 \n
+    Jose Diego Perez Toralla-201504081 \n
+    Herberth Abisai Avila Ruiz-201504464 \n
+
+  `);
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
