d4761979acdfa3c5bc40d7df06e9559dc89d9748
Grifiun <denii.grifon@gmail.com>
2023-06-12 23:39:35 -0600

    se agrego endpoint resta

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
+// Configuración para analizar datos JSON en las solicitudes POST
+app.use(express.json());
+
+// Endpoint para la resta
+app.post('/resta', (req, res) => {
+  // Obtén los valores de la solicitud POST
+  const { num1, num2 } = req.body;
+
+  // Realiza la resta
+  const resultado = num1 - num2;
+
+  // Devuelve el resultado como respuesta
+  res.json({ resultado });
+});
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
