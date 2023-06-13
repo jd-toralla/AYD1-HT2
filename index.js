18087ff4c1344f503ade27073ea18eb707766f0c
Camran1234 <Camran750@gmail.com>
2023-06-12 22:59:38 -0600

    [ADD] Suma

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const app = express();
 
 
 function areNumbers(num1, num2){
-    if(typeof num1 === 'number' && typeof num2 === 'number'){
-        return true;
-    }
-    return false;
+    return !isNaN(num1) && !isNaN(num2);
 }
 
 app.get('/', (req, res) => {
@@ -15,10 +12,13 @@ app.get('/', (req, res) => {
 
 //Suma
 app.post('/suma', (req, res) => {
-    const {num1, num2} = req.body;
+    
+    const {num1, num2} = req.query;
+    console.log(req.query)
     if(num1 != undefined && num2 !=undefined){
       if(areNumbers(num1, num2)){
-        res.send(`La suma es: ${num1 + num2}`);
+        const result = parseFloat(num1) + parseFloat(num2);
+        res.send(`La suma es: ${result}`);
       }else{
         res.send(`Error: Los valores ingresados no son números`);
       }
