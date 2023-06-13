db274c9dee548db39344309da57f7cdb03d37460
Camran1234 <Camran750@gmail.com>
2023-06-12 22:48:41 -0600

    [ADD] Suma

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 
+
+function areNumbers(num1, num2){
+    if(typeof num1 === 'number' && typeof num2 === 'number'){
+        return true;
+    }
+    return false;
+}
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
+
+//Suma
+app.post('/suma', (req, res) => {
+    const {num1, num2} = req.body;
+    if(num1 != undefined && num2 !=undefined){
+      if(areNumbers(num1, num2)){
+        res.send(`La suma es: ${num1 + num2}`);
+      }else{
+        res.send(`Error: Los valores ingresados no son números`);
+      }
+    }else{
+      res.send(`Error: No se ingresaron los valores`);
+    }
+});
+
 
 const port = 3000;
 app.listen(port, () => {
