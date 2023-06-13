6f1dc92be76638f9a92ec0d1007e2a1c92671b75
Diego Perez <dperez@somosproper.com>
2023-06-12 23:40:08 -0600

    Se agrego el endpoint primo que determina si un numero es primo o no segun el numero que recibe

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
 
+app.post('/primo-201504081/:numero', (req, res) => {
+    const numero = parseInt(req.params.numero);
+    res.send(`El número ${numero} ${esPrimo(numero) ? 'es primo' : 'no es primo'}`);
+  });
+  
+const esPrimo = (numero) => {
+    if (numero <= 1) return false;
+    for (let i = 2, sqrt = Math.sqrt(numero); i <= sqrt; i++)
+      if (numero % i === 0) return false;
+    return true;
+} 
+
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
